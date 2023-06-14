8b6cdbfcf2db4770a76747e4f2a85538da3c7ec8
Antonio Quendera <102910311+toneca2000@users.noreply.github.com>
2023-06-14 10:20:04 +0100

    NEECboto_semBluetooth.ino

--- NEECboto_semBluetooth.ino
+++ NEECboto_semBluetooth.ino
@@ -1,157 +1,131 @@
-#include <Servo.h>        //add Servo Motor library            
-#include <NewPing.h>      //add Ultrasonic sensor library
+#include <Servo.h>        // Biblioteca para controle do servo motor
+#include <NewPing.h>      // Biblioteca para o sensor ultrassônico
 
 #define TRIG_PIN 3 
 #define ECHO_PIN 4
-#define MAX_DISTANCE 300 // sets maximum useable sensor measuring distance to 300cm
-#define COLL_DIST 30 // sets distance at which robot stops and reverses to 30cm
-#define TURN_DIST COLL_DIST+20 // sets distance at which robot veers away from object
+#define MAX_DISTANCE 300 // Define a distância máxima de medição do sensor para 300cm
+#define COLL_DIST 25 // Define a distância em que o robô para e reverte o movimento para 30cm
+#define TURN_DIST COLL_DIST + 20 // Define a distância em que o robô desvia de um objeto
 
-NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); // sets up sensor library to use the correct pins to measure distance.
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); // Configura o sensor para usar os pinos corretos
 
-Servo left_servo;  // create servo object to control servos
-Servo right_servo;
-Servo reader;
+Servo left_servo;  // Criação do objeto para controle do servo motor esquerdo
+Servo right_servo; // Criação do objeto para controle do servo motor direito
+Servo reader; // Criação do objeto para controle do servo motor dos olhos
 
-//Global variables
-int leftDistance, rightDistance; //distances on either side
-int curDist = 0;
-int angle;
-int up;
-int a,b,c,p;
-//-------------------------------------------- SETUP LOOP ----------------------------------------------------------------------------
+// Variáveis globais
+int leftDistance, rightDistance; // Distâncias em cada lado
+int angle = 0;
+int angleIncrement = 50;
+
+//-------------------------------------------- CONFIGURAÇÃO --------------------------------------------------------
+
 void setup() {
   left_servo.attach(11); 
   right_servo.attach(10);
   reader.attach(6);
   reader.write(90);
   Serial.begin(9600);
-  delay(100); // delay for one seconds
-  angle=90;
-  up=1;
- }
-//------------------------------------------------------------------------------------------------------------------------------------
+  delay(1000); // Aguarda 1 segundo
+}
 
-//---------------------------------------------MAIN LOOP ------------------------------------------------------------------------------
+//--------------------------------------------- LOOP PRINCIPAL ----------------------------------------------------
+
 void loop() {
-  reader.write(angle);  // move eyes forward
-  a= readPing();
-  //b= readPing();
-  //c= readPing();
-
-  p=a;
-  
-  /*if(((a < c) and ( a>b))or((a > c) and ( a<b))){
-    p = a;
-  }else if(((b<c)and(b>a))or((b>c)and(b<a))){
-    p=b;
-  }else if(((c<a) and (c>b))or((c>a) and (c<b))){
-    p=c;
-  }else if( b == c){
-    p=b;
-  }*/
-  if ( p < COLL_DIST) {
+  reader.write(angle);  
+  if (readPing() < COLL_DIST) {
     reader.write(90);
     changePath();
-  }  // if forward is blocked change direction
-  else {
-    moveForward();  // move forward
+  } else {
+    moveForward();
   }
-  //delay(500);
- }
-//-----------------------------------------------------------//Deciding which side to go//------------------------------------------------------------------------
+  
+  delay(100); 
+  
+  // new
+  angle += angleIncrement;
+  if (angle <= 0 || angle >= 100) {
+    angleIncrement *= -1;  
+  }
+}
+
+//------------------------------------------------- MÉTODOS -------------------------------------------------------
 
 void changePath() {
-  moveStop();   // stop forward movement
-    reader.write(36);  // check distance to the right
-    delay(300);
-    rightDistance = readPing(); //set right distance
-    delay(300);
-    reader.write(144);  // check distace to the left
-    delay(500);
-    leftDistance = readPing(); //set left distance
-    delay(300);
-    reader.write(90); //return to center
-    delay(100);
-    compareDistance();
-  }
+  moveStop();
+  reader.write(36);
+  delay(300); // Atraso adicional para estabilizar o servo motor antes de ler a distância à direita
+  rightDistance = readPing();
+  delay(300); // Atraso adicional antes de ler a distância à esquerda
+  reader.write(144);
+  delay(300); // Atraso adicional para estabilizar o servo motor antes de ler a distância à esquerda
+  leftDistance = readPing();
+  delay(300);
+  reader.write(90);
+  delay(300);
+  compareDistance();
+}
 
-  
-void compareDistance()   // find the longest distance
-{
-  if (leftDistance>rightDistance  and leftDistance>30) //if left is less obstructed 
-  {
+void compareDistance() {
+  if (leftDistance > rightDistance && leftDistance > 25) {
     turnLeft();
-  }
-  else if (rightDistance>leftDistance and rightDistance>30) //if right is less obstructed
-  {
+  } else if (rightDistance > leftDistance && rightDistance > 25) {
     turnRight();
-  }
-   else  if (leftDistance>15 and rightDistance>15) //if they are equally obstructed
-  {
+  } else if (leftDistance < 15 && rightDistance < 15) {
     turnAround();
   } else {
-
     moveBackward();
+    delay(500); // Aumenta o tempo de movimento para trás para 2 segundos (ou ajuste conforme necessário)
     changePath();
-    
   }
 }
 
-
-//--------------------------------------------//Reading the distance//-------------------------------------------------------------------------------------
-
-int readPing() { // read the ultrasonic sensor distance
-  delay(50);   
-  unsigned int uS = sonar.ping();
-  int cm = uS/US_ROUNDTRIP_CM;
-  if(cm == 0){
-    
-    cm = MAX_DISTANCE;
+int readPing() {
+  delay(50); // Atraso para 50 milissegundos
+  int distance = sonar.ping_cm();
+  if (distance == 0) {
+    distance = MAX_DISTANCE;
   }
-  Serial.println(cm);
-  return cm;
+  Serial.println(distance);
+  return distance;
 }
-//------------------------------------------------//movement---------------------------------------------------------------------------------
-void moveStop() { right_servo.write(90); left_servo.write(90);}  // stop the motors.
-//-------------------------------------------------------------------------------------------------------------------------------------
+
+void moveStop() {
+  right_servo.write(90);
+  left_servo.write(90);
+}
+
 void moveForward() {
-  right_servo.write(75);
-  left_servo.write(109);
+  right_servo.write(60);
+  left_servo.write(122);
 }
-//-------------------------------------------------------------------------------------------------------------------------------------
+
 void moveBackward() {
-    right_servo.write(105);
-    left_servo.write(71);
-    delay(1000);
-}  
-//-------------------------------------------------------------------------------------------------------------------------------------
+  right_servo.write(120);
+  left_servo.write(58);
+  delay(300); // Aumenta o tempo de movimento para trás para 2 segundos (ou ajuste conforme necessário)
+}
+
 void turnRight() {
-  left_servo.write(105);
-  right_servo.write(105);
-  delay(500); // run motors this way for 1500        
+  left_servo.write(122);
+  right_servo.write(120);
+  delay(500);
+  right_servo.write(0); // Define os motores de volta para a frente
+  left_servo.write(180);
+}
 
-   right_servo.write(90); //Set motors back to forward
-   left_servo.write(90); 
-   delay(200);
-}  
-//-------------------------------------------------------------------------------------------------------------------------------------
 void turnLeft() {
-  left_servo.write(75);
-  right_servo.write(75);
-  delay(500); // run motors this way for 1500     
-  right_servo.write(90); //Set motors back to forward
-  left_servo.write(90); 
-  delay(200);
-}  
-//-------------------------------------------------------------------------------------------------------------------------------------
-void turnAround() {
-  left_servo.write(110);
-  right_servo.write(110);
-  delay(1000); // run motors this way for 1700        
+  left_servo.write(58);
+  right_servo.write(60);
+  delay(500);
+  right_servo.write(0); // Define os motores de volta para a frente
+  left_servo.write(180);
+}
 
-  right_servo.write(90); //Set motors back to forward
-  left_servo.write(90); 
-}  
-//--------------------------------------------------------------------------------------------------------------------------------------
-//void 
+void turnAround() {
+  left_servo.write(140);
+  right_servo.write(140);
+  delay(1000);
+  right_servo.write(90); // Define os motores de volta para a frente
+  left_servo.write(90);
+}
